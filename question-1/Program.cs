7030f830b42d5cf961e429e5458b44065fc8012b
Jefferson Duarte <jeffersonsduarte@live.com>
2024-04-23 15:57:11 +0100

    oop-exam-71205

---
+++ question-1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class BuildingCodes
+{
+    public int buildindId;
+    public string buildingCode;
+    public string buildingLocation;
+
+    public BuildingCodes(int id, string code, string location)
+    {
+        buildindId = id;
+        buildingCode = code;
+        buildingLocation = location;
+    }
+
+    public BuildingCodes()
+    {
+    }
+
+    List<BuildingCodes> buildingCodes = new List<BuildingCodes>();
+
+    public void AddBuildingCode(int id, string code, string location)
+    {
+        buildingCodes.Add(new BuildingCodes(id, code, location));
+    }
+
+    public void DisplayBuildingCodes()
+    {
+        foreach (BuildingCodes buildingCode in buildingCodes)
+        {
+            Console.WriteLine("Building ID: " + buildingCode.buildindId);
+            Console.WriteLine("Building Code: " + buildingCode.buildingCode);
+            Console.WriteLine("Building Location: " + buildingCode.buildingLocation);
+            Console.WriteLine("---------------------------------------------------");
+        }
+    }
+}
+
+class Program 
+{ 
+    static void Main()
+    {
+        BuildingCodes buildingCodes = new BuildingCodes();
+
+        for (int i = 0; i < 10; i++)
+        {
+            buildingCodes.AddBuildingCode(
+                Faker.RandomNumber.Next(1, 100),
+                Faker.RandomNumber.Next(1, 100).ToString(),
+                Faker.Company.Name()
+            );
+        }
+
+        Console.WriteLine("Descending Order of Building Codes");
+
+        buildingCodes.DisplayBuildingCodes();
+        
+    }
+}
---
+++ question-3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Buffers.Text;
+class Movie
+{
+    public string name;
+    public string rating;
+    public double runninTime; 
+    public string director;
+
+    public Movie(string name, string rating, double runninTime, string director)
+    {
+        this.name = name;
+        this.rating = rating;
+        this.runninTime = runninTime;
+        this.director = director;
+    }
+    public Movie()
+    {
+    }
+
+    List<Movie> movies = new List<Movie>();
+
+    public void AddMovie(string name, string rating, double runninTime, string director)
+    {
+        movies.Add(new Movie(name, rating, runninTime, director));
+    }
+    
+    public void DisplayMovies()
+    {
+        foreach (Movie movie in movies)
+        {
+            Console.WriteLine("Movie Name: " + movie.name);
+            Console.WriteLine("Movie Rating: " + movie.rating);
+            Console.WriteLine("Movie Running Time: " + movie.runninTime);
+            Console.WriteLine("Movie Director: " + movie.director);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Movie movies = new Movie();
+        movies.AddMovie("Movie 1", "PG-13", 2.5, "Director 1");
+        movies.AddMovie("Movie 2", "PG-13", 2.5, "Director 2");
+        movies.AddMovie("Movie 3", "PG-13", 2.5, "Director 3");
+        movies.DisplayMovies();
+    }
+}
